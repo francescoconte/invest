2592c6d936075f51ec0acfc2fada66ad49da363b
francescolucania <francescolucania@be-simple.su>
2016-07-23 20:45:00 +0300

    pre@

---
+++ public/js/main.js
@@ -0,0 +1,23 @@
+ $(document).ready(function() {
+ var lang = 0;
+ 
+ 
+$('div#langs').click(function() {
+    
+if (lang == 0) {
+$('#eng').addClass('active');   
+$('#rus').removeClass('active'); 
+lang = 1;} 
+    
+    
+    else {
+$('#rus').addClass('active');   
+$('#eng').removeClass('active'); 
+lang = 0;
+}     
+      
+    
+});     
+     
+     
+});
