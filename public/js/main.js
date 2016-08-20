d3de4b86d278cefadf83114b3d8a88f14dd4d264
francescolucania <francescolucania@be-simple.su>
2016-08-20 18:56:11 +0300

    адаптив к первой

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,47 @@
  $(document).ready(function() {
- var lang = 0;
+ var lang = 0; widthPage =  $(window).width();
+  
      
- 
- 
+
+     
+     
+ if (widthPage > 999) {
+
+ $('#fullpage').fullpage({
+anchors: ['home-1', 'home-2', 'home-3', 'home-4', 'home-5', 'footer'],
+menu: '#menu',
+scrollingSpeed: 1000
+			}); 
+     
+     
+ }
+     
+
+     
+
+$('#menu').click(function() {  
+$('#sub2').addClass('active-sub');
+}); 
+     
+
+$('#close-menu').click(function() {
+
+$('#sub2').removeClass('active-sub'); 
+}); 
+    
+     
+     
+     
+
+$('#search').click(function() {
+$('#sub1').addClass('active-sub');
+}); 
+
+$('#close-search').click(function() {
+$('#sub1').removeClass('active-sub'); 
+}); 
+
+
 $('div#langs').click(function() {
     
 if (lang == 0) {
